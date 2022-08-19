fd815c84114b86ffd464a57a16ce198e2f750773
jvcano <iorijvc@gmail.com>
2022-08-18 21:06:52 -0400

    this is the headless version of the same code

--- part_test.py
+++ part_test.py
@@ -6,10 +6,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.options import Options
 
 curr_date = date.today()
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+option.add_argument("window-size=1980x960")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
 driver.get(config.url)
 driver.maximize_window()
 sleep(1)
@@ -19,10 +22,11 @@ driver.find_element(By.NAME,"txtPassword").send_keys(config.password)
 driver.find_element(By.NAME,"btnLogin").click()
 sleep(1)
 driver.switch_to.frame('aframe')
-sleep(1)
-#driver.find_element_by_id('inbox_inboxoutboxGrid_ctl00_ctl10_hypGetDetails').click()
-#var = driver.find_element(By.XPATH,"//table[@id='inbox_inboxoutboxGrid_ctl00']").text
-#print(var)
+sleep(2)
+
+
+####################
+###holidays and other working 
 driver.find_element(By.ID,'inbox_inboxoutboxGrid_ctl00_ctl10_hypGetDetails').click() 
 sleep(2)
 driver.find_element(By.ID,'pnlTimesheet')
@@ -37,5 +41,48 @@ driver.find_element(By.XPATH,"/html[1]/body[1]/form[1]/table[1]/tbody[1]/tr[1]/t
 sleep(2)
 driver.find_element(By.XPATH,'/html[1]/body[1]/form[1]/div[2]/table[1]/tbody[1]/tr[2]/td[1]/a[1]').click()
 sleep(2)
+var = driver.find_element((By.XPATH,'/html[1]/body[1]/form[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[3]/td[3]/select[1]')).text
 
 driver.find_element(By.XPATH,'/html[1]/body[1]/form[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[3]/td[3]/select[1]').click()
+sleep(2)
+driver.get_screenshot_as_file(f'saturday.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" var=driver.find_element(By.XPATH,"//a[@id='outbox_inboxoutboxGrid_ctl00_ctl04_hypGetDetails']").text
+var3=driver.find_element(By.XPATH,"/html[1]/body[1]/form[1]/table[1]/tbody[1]/tr[8]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[4]").text
+var2=driver.find_element(By.XPATH,"//tr[@id='outbox_inboxoutboxGrid_ctl00__0']//td[3]").text
+var4=driver.find_element(By.XPATH,'/html[1]/body[1]/form[1]/table[1]/tbody[1]/tr[8]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[7]').text
+driver.save_screenshot('Sunday.png') """
+#driver.get_screenshot_as_file(f'Sunday.png')
+""" 
+print(var +" "+ var2 +" "+ var3 +' ' + var4)
+
+file = open("config.py", "a")
+var = repr(var)
+var2 = repr(var2)      
+var3 = repr(var3)
+var4 = repr(var4)
+file.write("\n" + "date = " +f"{var}" + "\n" +"name = "+f'{var2}' + "\n"+"status = "+f'{var3}'+ "\n"+"hours="+f'{var4}' )
+file.close """
+
--- test_holidays.py
+++ test_holidays.py
@@ -4,37 +4,44 @@ import holidays
 
 curr_date = date.today()
 print(curr_date)
+print(type(curr_date))
 print(calendar.day_name[curr_date.weekday()])
+check_holidays = '2023-01-01'
 
 today_date = calendar.day_name[curr_date.weekday()]
+print(today_date)
 # Select country
 US_holidays = holidays.US()
-if  calendar.day_name[curr_date.weekday()] == "Monday":
+if  today_date == "Monday":
     if curr_date in US_holidays == True:
         print("today is a monday and a holiday")
     else:
         print("is just a monday")
-elif calendar.day_name[curr_date.weekday()] == "Tuesday":
-    if curr_date in US_holidays == True:
+elif today_date== "Tuesday":
+    if today_date== True:
         print("today is a Tuesday and a holiday")
     else:
         print("is just a Tuesday")
-elif calendar.day_name[curr_date.weekday()] == "Wednsday":
+elif today_date == "Wednsday":
     if curr_date in US_holidays== True:
         print("today is a Wednsday and a holiday")
     else:
         print("is just a Wednsday")
-elif calendar.day_name[curr_date.weekday()] == "Thursday":
+elif today_date == "Thursday":
     if curr_date in US_holidays== True:
         print("today is a Thursday and a holiday")
     else:
         print("is just a Thursday")
-elif calendar.day_name[curr_date.weekday()] == "Friday":
+elif today_date == "Friday":
     if curr_date in US_holidays== True:
         print("today is a Friday and a holiday")
     else:
         print("is just a Friday")
-    
+elif today_date == "Sunday":
+    if curr_date in US_holidays== True:
+        print("today is a Sunday and a holiday")
+    else:
+        print("is just a Sunday")    
 else:
     print("Not a week day")
 
